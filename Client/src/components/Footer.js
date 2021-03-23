1810e24012d59cbbcf68c5c21769f91728bad022
Lucia <luciag.lara@gmail.com>
2021-03-23 14:37:20 +0100

    nada

--- Client/src/components/Footer.js
+++ Client/src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Images from '../images/images'
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 
 const Footer = () => {
